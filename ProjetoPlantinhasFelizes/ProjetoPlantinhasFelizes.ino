f2a1eb37677860a870b00d39478e217c85e564d4
loryenipsum <lolofns@gmail.com>
2018-06-22 11:30:44 -0300

    Alterações para o código

--- ProjetoPlantinhasFelizes/ProjetoPlantinhasFelizes.ino
+++ ProjetoPlantinhasFelizes/ProjetoPlantinhasFelizes.ino
@@ -1,23 +1,24 @@
 /*
-  Analog input, analog output, serial output
+  ___ _          _   _      _              ___    _ _           _ _ _ 
+ | _ \ |__ _ _ _| |_(_)_ _ | |_  __ _ ___ | __|__| (_)______ __| | | |
+ |  _/ / _` | ' \  _| | ' \| ' \/ _` (_-< | _/ -_) | |_ / -_|_-<_|_|_|
+ |_| |_\__,_|_||_\__|_|_||_|_||_\__,_/__/ |_|\___|_|_/__\___/__(_|_|_)
 
-  Reads an analog input pin, maps the result to a range from 0 to 255 and uses
-  the result to set the pulse width modulation (PWM) of an output pin.
-  Also prints the results to the Serial Monitor.
+  Este projeto foi feito pra uma disciplina do IME-USP no 1o semestre
+  de 2018. É um sistema de irrigação automático de plantinhas de casa
+  que envia dados ao PubNub para monitoramento remoto.
 
-  The circuit:
-  - potentiometer connected to analog pin 0.
-    Center pin of the potentiometer goes to the analog pin.
-    side pins of the potentiometer go to +5V and ground
-  - LED connected from digital pin 9 to ground
+  É um sensor de umidade ligado ao Arduíno por entrada analógica (A0)
+  Que se liga a um relé que abre uma válvula solenóide ligada à água.
+  
+  Feito por Lourenço Fernandes Neto e Silva,
+  Com agradecimentos muitíssimo especiais a Júlio Turolla, namorido :)
+  Também a Antônio Deusany (DJ) e Marcelo Schmidtt, que conheci pela
+  disciplina referida: MAC6929 - Introdução à Internet das Coisas.
 
-  created 29 Dec. 2008
-  modified 9 Apr 2012
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/AnalogInOutSerial
+  (Talvez a primeira, sem dúvida uma das primeiras vezes que um curso
+  desse tema é dado no Brasil.)
+  
 */
 #include <ESP8266WiFi.h>
 #include <SPI.h>
@@ -25,113 +26,130 @@
 #define PUBNUB_DEBUG
 #include <PubNub.h>
 
+const char* redeWifi = "lalalala";
+const char* pass = "txurururururu";
 
-// Wireless settings
-const char* ssid = "wifiwifiwifiwifi";
-const char* pass = "passpasspasspass";
+const static char pubkey[] = "huhuhuhuhuhu";
+const static char subkey[] = "galopeeeeeeeeeeeee";
+const static char channel[] = "plantinhas-felizes";
 
-const static char pubkey[] = "txurururu";         // your publish key 
-const static char subkey[] = "yeyeyeyeye";         // your subscribe key
-const static char channel[] = "plantinhas-felizes"; // channel to use
-
-int status = WL_IDLE_STATUS; // the Wifi radio's status
+int status = WL_IDLE_STATUS;
 
 WiFiClient client;
 
-// These constants won't change. They're used to give names to the pins used:
 const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
-const int valveOpener = 0;
+const int valveOpener = 4;   // O que significa o pino D2 do Arduino
 
 int sensorValue = 0;        // value read from the pot
 
 void setup() {
-  // initialize serial communications at 9600 bps:
   Serial.begin(9600);
-    pinMode(0, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
-     wifiConnection();
-     PubNub.begin(pubkey, subkey);
+  pinMode(valveOpener, OUTPUT);
+  wifiConnection();
+  PubNub.begin(pubkey, subkey);
+  printLogo();
 }
 
 void openValve() {
-    digitalWrite(valveOpener, HIGH);
-    Serial.println("OPEN");
+  digitalWrite(valveOpener, HIGH);
+  Serial.println("OPEN");
 }
 
 void closeValve() {
-    digitalWrite(valveOpener, LOW);
-    Serial.println("CLOSED");
+  digitalWrite(valveOpener, LOW);
+  Serial.println("CLOSED");
 }
 
-void loop() {           
+void loop() {
   sensorValue = analogRead(analogInPin);
 
   Serial.print("sensor = ");
   Serial.println(sensorValue);
 
-  if (sensorValue > 500) {
+  if (sensorValue > 450) {
     Serial.println("valor alto demais!");
     return;
   }
+
   if (sensorValue < 0) {
     Serial.println("valor baixo demais!");
     return;
   }
-  if (sensorValue > 80) {
+
+  if (sensorValue > 100) {
     openValve();
-  } else {
+    delay(200);
     closeValve();
   }
 
-   if (WiFi.status() != WL_CONNECTED) {
-        wifiConnection();
-    } else {
-      delay(1000);
-      sendMessage(sensorValue);
-    }
-  
-  delay(3000);
+  if (WiFi.status() != WL_CONNECTED) {
+    wifiConnection();
+    sendMessage(sensorValue);
+  } else {
+    sendMessage(sensorValue);
+  }
+
+  delay(10000);
 }
 
 void sendMessage(int sensorValue) {
-   WiFiClient *client;
+  WiFiClient *client;
 
-      String message = String("\"" + String(sensorValue) + "\"");
-      char msg[5];
-      message.toCharArray(msg, 5);
-    
-      client = PubNub.publish(channel, msg);
-      
-      if (!client) {
-          Serial.println("publishing error");
-          delay(1000);
-          return;
-      }
-      
-      if (PubNub.get_last_http_status_code_class() != PubNub::http_scc_success) {
-          Serial.print("Got HTTP status code error from PubNub, class: ");
-          Serial.print(PubNub.get_last_http_status_code_class(), DEC);
-      }
-      
-      while (client->available()) {
-          Serial.write(client->read());
-      }
-      
-      client->stop();
-      Serial.println("---");
+  String message = String("\"" + String(sensorValue) + "\"");
+  char msg[20];
+  message.toCharArray(msg, 20);
+
+  client = PubNub.publish(channel, msg);
+
+  if (!client) {
+    Serial.println("publishing error");
+    delay(1000);
+    return;
+  }
+
+  if (PubNub.get_last_http_status_code_class() != PubNub::http_scc_success) {
+    Serial.print("Got HTTP status code error from PubNub, class: ");
+    Serial.print(PubNub.get_last_http_status_code_class(), DEC);
+  }
+
 }
 
-
 void wifiConnection() {
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, pass);
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
+  Serial.print("Connecting to ");
+  Serial.println(redeWifi);
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(redeWifi, pass);
+
+  int count = 0;
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    count++;
+    
+    if (count > 120) {
+      break;
     }
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());
+  }
+  
+  printIp();
+}
+
+void printIp() {
+  if (WiFi.status() != WL_CONNECTED) {
+    return;
+  }
+  
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void printLogo() {
+  Serial.println("  ___ _          _   _      _              ___    _ _           _ _ _ ");
+  Serial.println(" | _ \ |__ _ _ _| |_(_)_ _ | |_  __ _ ___ | __|__| (_)______ __| | | |");
+  Serial.println(" |  _/ / _` | ' \  _| | ' \| ' \/ _` (_-< | _/ -_) | |_ / -_|_-<_|_|_|");
+  Serial.println(" |_| |_\__,_|_||_\__|_|_||_|_||_\__,_/__/ |_|\___|_|_/__\___/__(_|_|_)");
 }
